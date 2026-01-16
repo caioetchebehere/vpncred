9e5712f938354a327272ea5d929229d881eda1e9
Caio Alexandre Camargo Etchebehere <126330067+caioetchebehere@users.noreply.github.com>
2026-01-16 09:19:38 -0300

    Delete api directory

--- api/test.js
+++
@@ -1,8 +0,0 @@
-// Arquivo de teste para verificar se o Vercel está detectando as funções
-module.exports = async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    return res.status(200).json({ 
-        message: 'API funcionando!',
-        timestamp: new Date().toISOString()
-    });
-};
